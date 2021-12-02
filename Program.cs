0d2210baaa9382af83cec3527b6394b68b47e21d
Deku <rockstar8577@users.noreply.github.com>
2021-12-01 20:34:23 -0500

    Update Program.cs
    Separate startup into multiple functions
    -Bot fully logs into market with specified user and keys
    
    *Still an error pops up with an element being missed sometimes

--- Program.cs
+++ Program.cs
@@ -11,164 +11,39 @@ namespace RentalBot
 {
     class Program
     {
+        private static IWebDriver driver;
+        private static WebDriverWait wait;
+        private static ChromeOptions options;
+
+        private static string username = "***REMOVED***";
+        private static string pwd = "***REMOVED***";
+        private static string postKey = "***REMOVED***";
+        private static string activeKey = "***REMOVED***";
+
         static void Main(string[] args)
         {
-            string username = "***REMOVED***";
-            string pwd = "***REMOVED***";
-            string postKey = "***REMOVED***";
-            string activeKey = "***REMOVED***";
-
             Console.WriteLine("Starting bot");
 
-            ChromeOptions options = new ChromeOptions();
+            options = new ChromeOptions();
+            // Import HiveWallet Chrome Extension
+            options.AddExtension("C:\\Splinterlands\\Scraper\\HIVE Extension\\1.14.2_0.crx");
+            //If using separate non-temp profiles
             //options.AddArgument("--user-data-dir=C:\\Splinterlands\\Profiles");
             //options.AddArgument("--profile-directory=Bot");
-            options.AddExtension("C:\\Splinterlands\\Scraper\\HIVE Extension\\1.14.2_0.crx");
 
-            using (IWebDriver driver = new ChromeDriver("C:\\Splinterlands", options))
+            using (driver = new ChromeDriver("C:\\Splinterlands", options))
             {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                driver.Navigate().GoToUrl("chrome-extension://jcacnejopjdphbnjgfaaobbfafkihpep/html/popup.html");
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
 
-                /*IWebElement walletPWD = wait.Until<IWebElement>(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("input[id='master_pwd']"));
-                        return (element.Displayed && element.Enabled) ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });*/
-                IWebElement walletPWD = FindElementDisplayedAndEnabled(driver, "input[id='master_pwd']");
-                walletPWD.SendKeys(pwd);
-                driver.FindElement(By.CssSelector("input[id='confirm_master_pwd']")).SendKeys(pwd);
-                driver.FindElement(By.CssSelector("button[id='submit_master_pwd']")).Click();
-
-                IWebElement addKeysChoice = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("button[id='add_by_keys']"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });
-                addKeysChoice.Click();
-
-                IWebElement addKeyUser = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("input[id='username']"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });
-                addKeyUser.SendKeys(username);
-                driver.FindElement(By.CssSelector("input[id='pwd']")).SendKeys(postKey);
-                driver.FindElement(By.CssSelector("button[id='check_add_account']")).Click();
-
-                wait.Until(conditions => conditions.FindElement(By.CssSelector("img[id='settings']")).Displayed);
-                driver.FindElement(By.CssSelector("img[id='settings']")).Click();
-                driver.FindElement(By.CssSelector("div[id='manage']")).Click();
-                wait.Until(conditions => conditions.FindElement(By.CssSelector("span[id='active_key_title']")).Displayed);
-                driver.FindElement(By.CssSelector("span[id='active_key_title']")).Click();
-                driver.FindElement(By.CssSelector("input[id='new_key']")).SendKeys(activeKey);
-                driver.FindElement(By.CssSelector("button[id='add_new_key']")).Click();
-
-                driver.Navigate().GoToUrl("https://peakmonsters.com/rentals");
-
-                IWebElement closeButton = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector(".is-visible svg"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        Console.WriteLine("Error retrieving close button");
-                        return null;
-                    }
-                });
-                closeButton.Click();
-
-                driver.FindElement(By.LinkText("Login")).Click();
-
-                IWebElement usernameInput = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector(".has-feedback-left .form-control, .has-feedback-left.input-group .form-control"));
-                        return element.Displayed ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        Console.WriteLine("Error retrieving username input");
-                        return null;
-                    }
-                });
-
-                usernameInput.Click();
-                usernameInput.SendKeys(username);
-                {
-                    int previousWinCount = driver.WindowHandles.Count;
-
-                    driver.FindElement(By.CssSelector("button[class='btn btn-block no-padding']")).Click();
-
-                    bool popupLoaded = wait.Until(conditions => conditions.WindowHandles.Count == previousWinCount + 1);
-                    driver.SwitchTo().Window(driver.WindowHandles.Last());
-                    IWebElement element = driver.FindElement(By.CssSelector("input[id='keep']"));
-                    element.Click();
-                    element = driver.FindElement(By.CssSelector("button[id='proceed']"));
-                    element.Click();
-                }
-
-                /*IWebElement keepCheckbox = wait.Until<IWebElement>(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("input[id='keep']"));
-                        return (element.Displayed) ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        Console.WriteLine("Error retrieving username input");
-                        return null;
-                    }
-                });*/
-                //keepCheckbox.Click();
-
-                //driver.FindElement(By.CssSelector("button[id='proceed']")).Click();
-
-                //Thread.Sleep(2000);
-                //wait.Until(webDriver => driver.FindElement(By.LinkText("Login")).Displayed);
-
-                //Thread.Sleep(10000);
-                //driver.FindElement*
-                //wait.Until(driver => driver.FindElement())
-                //wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
+                SetupWallet();
+                LoadRentals();
 
                 string line = "";
-                Console.WriteLine(@"1. CssSelector
-2. Frame List
-3. Window List
-4. Type END to quit");
+                Console.WriteLine(
+                        "1. CssSelector\n" +
+                        "2. Frame List\n" +
+                        "3. Window List\n" +
+                        "4. Type END to quit");
 
                 while ((line = Console.ReadLine()).ToUpper() != "END")
                 {
@@ -177,9 +52,35 @@ namespace RentalBot
                         case "1":
                             try
                             {
+                                line = "";
                                 line = Console.ReadLine();
                                 IWebElement element = driver.FindElement(By.CssSelector(line));
                                 Console.WriteLine($"Found element: {element}");
+                                try
+                                {
+                                    Console.WriteLine(
+                                        "Which command would you like to try?\n" +
+                                        "0. None\n" +
+                                        "1. Click");
+                                    line = Console.ReadLine();
+                                    if (line == "1")
+                                    {
+                                        element.Click();
+                                    }
+                                    else if(line == "2")
+                                    {
+                                        Console.WriteLine($"Element text: {element.Text}");
+                                    }
+                                    else if(line == "3")
+                                    {
+                                        Console.WriteLine($"Element attributes: {element}");
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine($"Error: {e}");
+                                    Console.WriteLine($"Can't perform action: {line}");
+                                }
                             }
                             catch (Exception e)
                             {
@@ -187,6 +88,7 @@ namespace RentalBot
                                 Console.WriteLine($"Can't find element: {line}");
                             }
                             break;
+
                         case "2":
                             IList<IWebElement> elements = driver.FindElements(By.TagName("frame"));
                             //System.out.println("Number of frames in a page :" + ele.size());
@@ -198,6 +100,7 @@ namespace RentalBot
                                 Console.WriteLine("Frame name :" + ele.GetAttribute("name"));
                             }
                             break;
+
                         case "3":
                             IList<string> windows = driver.WindowHandles;
                             //System.out.println("Number of frames in a page :" + ele.size());
@@ -211,7 +114,7 @@ namespace RentalBot
                             try
                             {
                                 driver.SwitchTo().Window(driver.WindowHandles.Last());
-                                driver.FindElement(By.CssSelector("input[id='keep']")).Click();
+                                //driver.FindElement(By.CssSelector("input[id='keep']")).Click();
                             }
                             catch (Exception e)
                             {
@@ -226,89 +129,79 @@ namespace RentalBot
                     }
 
 
-                    Console.WriteLine(@"1. CssSelector
-2. Frame List
-3. Window List
-4. Type END to quit");
+                    Console.WriteLine(
+                        "1. CssSelector\n" +
+                        "2. Frame List\n" +
+                        "3. Window List\n" +
+                        "4. Type END to quit");
                 }
             }
 
             Console.WriteLine("Ending bot");
             Thread.Sleep(3000);
         }
-        void SetupWallet(string username, string pwd, string postKey, string activeKey)
+        static void SetupWallet()
         {
             Console.WriteLine("Setting up wallet");
 
-            ChromeOptions options = new ChromeOptions();
-            options.AddExtension("C:\\Splinterlands\\Scraper\\HIVE Extension\\1.14.2_0.crx");
+            // Navigate to in-browser html page for extension
+            driver.Navigate().GoToUrl("chrome-extension://jcacnejopjdphbnjgfaaobbfafkihpep/html/popup.html");
 
-            using (IWebDriver driver = new ChromeDriver("C:\\Splinterlands", options))
+            // Setup wallet password
+            FindElementDisplayedAndEnabled("input[id='master_pwd']").SendKeys(pwd);
+            FindElementDisplayedAndEnabled("input[id='confirm_master_pwd']").SendKeys(pwd);
+            FindElementDisplayedAndEnabled("button[id='submit_master_pwd']").Click();
+
+            // Add account username and key(posting key)
+            FindElementDisplayedAndEnabled("button[id='add_by_keys']").Click();
+            FindElementDisplayedAndEnabled("input[id='username']").SendKeys(username);
+            FindElementDisplayedAndEnabled("input[id='pwd']").SendKeys(postKey);
+            FindElementDisplayedAndEnabled("button[id='check_add_account']").Click();
+
+            // Add key(active key)
+
+            //wait.Until(conditions => conditions.FindElement(By.CssSelector("img[id='settings']")).Displayed);
+
+            FindElementDisplayedAndEnabled("img[id='settings']").Click();
+            FindElementDisplayedAndEnabled("div[id='manage']").Click();
+
+            //wait.Until(conditions => conditions.FindElement(By.CssSelector("span[id='active_key_title']")).Displayed);
+
+            FindElementDisplayedAndEnabled("div[style='display: block;']").Click();
+            FindElementDisplayedAndEnabled("input[id='new_key']").SendKeys(activeKey);
+            FindElementDisplayedAndEnabled("button[id='add_new_key']").Click();
+        }
+
+        static void LoadRentals()
+        {
+            Console.WriteLine("Loading rental page");
+
+            // Navigate to rental page
+            driver.Navigate().GoToUrl("https://peakmonsters.com/rentals");
+
+            // Clear first popup
+            FindElementDisplayedAndEnabled(".is-visible svg").Click();
+
+            // Begin login process
+            driver.FindElement(By.LinkText("Login")).Click();
+            FindElementDisplayedAndEnabled(".has-feedback-left .form-control, .has-feedback-left.input-group .form-control").Click();
+            FindElementDisplayedAndEnabled(".has-feedback-left .form-control, .has-feedback-left.input-group .form-control").SendKeys(username);
             {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                driver.Navigate().GoToUrl("chrome-extension://jcacnejopjdphbnjgfaaobbfafkihpep/html/popup.html");
-
-                IWebElement walletPWD = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("input[id='master_pwd']"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });
-                walletPWD.SendKeys(pwd);
-                driver.FindElement(By.CssSelector("input[id='confirm_master_pwd']")).SendKeys(pwd);
-                driver.FindElement(By.CssSelector("button[id='submit_master_pwd']")).Click();
-
-                IWebElement addKeysChoice = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("button[id='add_by_keys']"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });
-                addKeysChoice.Click();
-
-                IWebElement addKeyUser = wait.Until(conditions =>
-                {
-                    try
-                    {
-                        IWebElement element = conditions.FindElement(By.CssSelector("input[id='username']"));
-                        return element.Displayed && element.Enabled ? element : null;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error: {e}");
-                        return null;
-                    }
-                });
-                addKeyUser.SendKeys(username);
-                driver.FindElement(By.CssSelector("input[id='pwd']")).SendKeys(postKey);
-                driver.FindElement(By.CssSelector("button[id='check_add_account']")).Click();
-
-                wait.Until(conditions => conditions.FindElement(By.CssSelector("img[id='settings']")).Displayed);
-                driver.FindElement(By.CssSelector("img[id='settings']")).Click();
-                driver.FindElement(By.CssSelector("div[id='manage']")).Click();
-                wait.Until(conditions => conditions.FindElement(By.CssSelector("span[id='active_key_title']")).Displayed);
-                driver.FindElement(By.CssSelector("span[id='active_key_title']")).Click();
-                driver.FindElement(By.CssSelector("input[id='new_key']")).SendKeys(activeKey);
-                driver.FindElement(By.CssSelector("button[id='add_new_key']")).Click();
+                //Wait for second window handle to appear to change focused window
+                int previousWinCount = driver.WindowHandles.Count;
+                FindElementDisplayedAndEnabled("button[class='btn btn-block no-padding']").Click();
+                bool popupLoaded = wait.Until(conditions => conditions.WindowHandles.Count == previousWinCount + 1);
+                driver.SwitchTo().Window(driver.WindowHandles.Last());
+                // Confirm wallet transaction and "Do Not Show Again" prompt
+                driver.FindElement(By.CssSelector("input[id='keep']")).Click();
+                driver.FindElement(By.CssSelector("button[id='proceed']")).Click();
+                driver.SwitchTo().Window(driver.WindowHandles.Last());
             }
         }
-        private static IWebElement FindElementDisplayedAndEnabled(IWebDriver driver, string css)
+
+        private static IWebElement FindElementDisplayedAndEnabled(string css, int seconds = 20)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
 
             return wait.Until(conditions =>
             {
